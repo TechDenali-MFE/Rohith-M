fd1cbd5048ef27c37ed5f40118e4d90410a551bb
Rohith Malgari <rohith.malgari@techdenali.com>
2026-06-22 19:44:25 +0530

    Added a event listener

--- src/App.tsx
+++ src/App.tsx
@@ -6,10 +6,22 @@ interface AppProps {
 }
 
 const Wizard: React.FC<AppProps> = ({ options }) => {
+  const handleIncrease = () => {
+    const event = new CustomEvent("increase-counter", {
+      detail: {
+        value: 1,
+      },
+    });
+
+    window.dispatchEvent(event);
+  };
   return (
     <div>
       <h2>Wizard</h2>
       <div>This is Rohith's Wizard {options?.name}</div>
+       <button onClick={handleIncrease}>
+        Add To Cart
+      </button>
     </div>
   );
 };
