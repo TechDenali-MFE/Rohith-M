d062569b88dd0ed25403df0d258a5e57cc564480
Rohith Malgari <rohith.malgari@techdenali.com>
2026-06-22 12:56:56 +0530

    modified config as per deployment

--- vite.config.ts
+++ vite.config.ts
@@ -2,30 +2,21 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
 
-export default defineConfig(({ mode }) => {
-  const isWidget = mode === "widget";
-
-  return {
-    plugins: [react()],
-
-    build: 
-       {
-          lib: {
-            entry: path.resolve(__dirname, "src/index.widget.tsx"),
-            name: "RohithWizard",
-            formats: ["iife"],
-            fileName: () => "bundle.js",
-          },
-          outDir: "dist",
-          emptyOutDir: true,
-          minify: "esbuild",
-          sourcemap: true,
-          cssCodeSplit: false,
-          rollupOptions: {
-            output: {
-              inlineDynamicImports: true,
-            },
-          },
-        }
-  };
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, "src/index.widget.tsx"),
+      name: "RohithWizard",
+      formats: ["iife"],
+      fileName: () => "bundle.js",
+    },
+    rollupOptions: {
+      external: [],
+    },
+    minify: true,
+  },
+  define: {
+    "process.env": {},
+  },
 });
